7271a22e5b1c4f2c1c4aaa95f0adefb8f31f9c02
ghandidev <ghandiaboultaif@gmail.com>
2024-05-09 18:59:40 -0400

    completed getHighestDuplicates()

--- script.js
+++ script.js
@@ -54,6 +54,34 @@ const getHighestDuplicates = (arr) => {
       counts[num] = 1;
     }
   }
+  let highestCount = 0;
+  for (let num of arr) {
+    const count = counts[num];
+    if (count >= 3 && count > highestCount) {
+      highestCount = count;
+    }
+    if (count >= 4 && count > highestCount) {
+      highestCount = count;
+    }
+  }
+  const sumOfAllDice = diceValuesArr.reduce((a, b) => a + b, 0);
+  if (highestCount >= 4) {
+    updateRadioOption(1, sumOfAllDice);
+  }
+  if (highestCount >= 3) {
+    updateRadioOption(0, sumOfAllDice);
+  }
+  updateRadioOption(5, 0);
+};
+
+const resetRadioOption = () => {
+  scoreInputs.forEach((input) => {
+    input.disabled = true;
+    input.checked = false;
+  });
+  scoreSpans.forEach((span) => {
+    span.textContent = '';
+  });
 };
 
 rulesBtn.addEventListener('click', () => {
@@ -72,7 +100,9 @@ rollDiceBtn.addEventListener('click', () => {
     alert('You have made three rolls this round. Please select a score.');
   } else {
     rolls++;
+    resetRadioOption();
     rollDice();
     updateStats();
+    getHighestDuplicates(diceValuesArr);
   }
 });
