9b8fb55b39310773224395724126d338084be6b4
ghandidev <ghandiaboultaif@gmail.com>
2024-05-09 14:47:19 -0400

    added variables

---
+++ script.js
@@ -0,0 +1,15 @@
+const listOfAllDice = document.querySelectorAll('.die');
+const scoreInputs = document.querySelectorAll('#score-options input');
+const scoreSpans = document.querySelectorAll('#score-options span');
+
+const currentRoundText = document.getElementById('current-round');
+const currentRoundRollsText = document.getElementById('current-round-rolls');
+
+const totalScoreText = document.getElementById('total-score');
+const scoreHistory = document.getElementById('score-history');
+
+const rollDiceBtn = document.getElementById('roll-dice-btn');
+const keepScoreBtn = document.getElementById('keep-score-btn');
+
+const rulesContainer = document.querySelector('.rules-container');
+const rulesBtn = document.getElementById('rules-btn');
