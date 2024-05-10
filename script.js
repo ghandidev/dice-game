3060f9c0d5015b130b9db785a326575270745b3c
ghandidev <ghandiaboultaif@gmail.com>
2024-05-09 21:25:00 -0400

    final commit added checkForStraights()

--- script.js
+++ script.js
@@ -80,6 +80,40 @@ const getHighestDuplicates = (arr) => {
   updateRadioOption(5, 0);
 };
 
+// Verifica si hay un posible escalera con un includes() ordenando los resultados primero con un Sort(),
+// luego los resultados 1 x 1 ingresan en un nuevo array usando .join()
+const checkForStraights = (arr) => {
+  const sortedNumbersArr = arr.sort((a, b) => a - b);
+  const uniqueNumbersArr = [...new Set(sortedNumbersArr)];
+  const uniqueNumbersStr = uniqueNumbersArr.join('');
+
+  const smallStraightsArr = ['1234', '2345', '3456'];
+  const largeStraightsArr = ['12345', '23456'];
+
+  if (
+    smallStraightsArr.some((straight) => uniqueNumbersStr.includes(straight))
+  ) {
+    updateRadioOption(3, 30);
+  }
+
+  if (largeStraightsArr.includes(uniqueNumbersStr)) {
+    updateRadioOption(4, 40);
+  }
+  updateRadioOption(5, 0);
+};
+
+const detectFullHouse = (arr) => {
+  const counts = {};
+  for (let num of arr) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+  // remember Object.values create an array with only counts and includes verify if that number is inside
+  const hasThreeOfAKind = Object.values(counts).includes(3);
+  const hasPair = Object.values(counts).includes(2);
+  if (hasThreeOfAKind && hasPair) updateRadioOption(2, 25);
+  updateRadioOption(5, 0);
+};
+
 const resetRadioOption = () => {
   scoreInputs.forEach((input) => {
     input.disabled = true;
@@ -154,5 +188,7 @@ rollDiceBtn.addEventListener('click', () => {
     rollDice();
     updateStats();
     getHighestDuplicates(diceValuesArr);
+    detectFullHouse(diceValuesArr);
+    checkForStraights(diceValuesArr);
   }
 });
